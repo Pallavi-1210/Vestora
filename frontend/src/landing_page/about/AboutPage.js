c85c88801d55a13610b0049d5c185327b46c5225
Pallavi <negipallavi2005@gmail.com>
2026-07-17 22:42:01 +0530

    Fix ESLint unused imports

--- frontend/src/landing_page/about/AboutPage.js
+++ frontend/src/landing_page/about/AboutPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
 import Team from "./Team";
-import Footer from "../Footer";
 
 function AboutPage() {
   return (
--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -4,8 +4,6 @@ import Awards from "./Awards";
 import Stats from "./Stats";
 import Pricing from "./Pricing";
 import OpenAccount from "../OpenAccount";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
 
 function HomePage() {
   return (

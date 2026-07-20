55fcf1372744655eb001e1b4c2ab4c8d57ec8845
Pallavi <negipallavi2005@gmail.com>
2026-07-20 22:03:12 +0530

    Fix dashboard ESLint unused imports

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,17 +1,10 @@
 import React, { useState, useContext } from "react";
 
-import axios from "axios";
-
 import GeneralContext from "./GeneralContext";
 
 import { Tooltip, Grow } from "@mui/material";
 
-import {
-  BarChartOutlined,
-  KeyboardArrowDown,
-  KeyboardArrowUp,
-  MoreHoriz,
-} from "@mui/icons-material";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 
 import { watchlist } from "../data/data";
 import { DoughnutChart } from "./DoughnoutChart";
